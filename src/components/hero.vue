2263541abcdfc364b0a65a374e0bbb035d78e4f2
giov <pintogiovanni818@gmail.com>
2021-09-17 16:51:10 +0200

    push 2

---
+++ src/components/hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Hero">
+    <img url="https://giocatoreperfetto.com/wp-content/uploads/2021/06/Madden-22-il-jumbotron-dei-cowboy-offre-un-vantaggio-nel.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
